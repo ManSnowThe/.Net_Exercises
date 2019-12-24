761501507a5b10222d2f0c7b56560dd4890687fa
Александр Ильин <ManSnowTh3@gmail.com>
2019-12-24 21:45:25 +0600

    Консольное приложение для чтения координат

---
+++ Exec1/Ex1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exec1
+{
+    class Ex1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X и Y координаты:");
+
+            decimal x = Convert.ToDecimal(Console.ReadLine());
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            string xy = (x + " " + y).ToString();
+            xy = xy.Replace(',', '.');
+            xy = xy.Replace(' ', ',');
+
+            //Console.WriteLine(xy);
+            Enter(xy);
+        }
+
+        static void Enter(string cord)
+        {
+            var split = cord.Split(',');
+
+            var str1 = split[0].Replace('.', ',');
+            var str2 = split[1].Replace('.', ',');
+
+            Console.WriteLine("X: {0} Y: {1}", str1, str2);
+        }
+    }
+}
