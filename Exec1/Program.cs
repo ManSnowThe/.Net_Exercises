db3927148138935c028388040ca8b87910e983eb
Александр Ильин <ManSnowTh3@gmail.com>
2019-12-24 20:13:00 +0600

    Добавьте файлы проекта.

---
+++ Exec1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
